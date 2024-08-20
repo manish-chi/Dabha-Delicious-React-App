9bbf998894f2ffcdd24b355f1ae7dfac4b15950a
manish-chi <chitre.manish@gmail.com>
2024-08-20 18:18:40 +0530

    changed login url

--- src/pages/login.js
+++ src/pages/login.js
@@ -54,7 +54,7 @@ function Login() {
     }
 
     try {
-      const url = "http://localhost:3000/api/v1/users/login";
+      const url = `${process.env.DHABA_DELICIOUS_BACKEND_URL}/api/v1/users/login`;
       const response = await fetch(url, {
         method: "POST",
         headers: {
